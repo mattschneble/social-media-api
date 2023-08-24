95aee6177f0df18479206f0a9e89a7908e3ac813
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-24 16:54:28 -0700

    built connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,10 @@
-// creating file
+// Require Mongoose
+const { connect, connection } = require('mongoose');
+
+// Connect to MongoDB
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmediaDB';
+
+connect(connectionString);
+
+module.exports = connection;
