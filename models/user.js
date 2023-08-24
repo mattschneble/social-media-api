282a35a1725ef3db664df31ef93d8d6fd405fe55
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-24 16:16:40 -0700

    created user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,54 @@
-// creating file
+// Create a User Model
+
+// Require Mongoose Schema and Model
+const { Schema, model } = require('mongoose');
+
+// Create User Schema
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+        },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // Use RegEx to validate correct email format
+        match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        },
+    // Link to the Thought model
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "thought",
+        },
+    ],
+    // self-reference to create a friends list
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+        },
+    ],
+},
+{
+    // Use virtuals
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+// Create an object to hold the User model methods
+const User = model("user", UserSchema);
+
+// Export the User model
+module.exports = User;
