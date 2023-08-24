05a8e40ccb10b1738b2f8b7a083e948260e96d98
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-24 16:40:36 -0700

    created reaction model and index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,9 @@
-// creating file
+// Import the User model
+const User = require('./user');
+// Import the Thought model
+const Thought = require('./thought');
+// Import the Reaction model
+const Reaction = require('./Reaction');
+
+// Export the User, Thought, and Reaction models
+module.exports = { User, Thought, Reaction };
--- models/reaction.js
+++ models/reaction.js
@@ -1,1 +1,38 @@
-// creating file
+// Create a Reaction Schema
+
+// Require Mongoose for creating the schema
+const mongoose = require('mongoose');
+
+// Create a Reaction Schema
+const ReactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // Use a getter method to format the timestamp on query
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    }
+},
+{
+    // Use toJSON to tell the schema to use getters
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
+
+// Export the Reaction Schema
+module.exports = ReactionSchema;
