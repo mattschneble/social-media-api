a3d6e1bd71e44622e3c37a16846d496cc2060257
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-25 17:07:27 -0700

    created route index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
-// Creating file
+// Import express router
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// Set up router middleware
+router.use('/api', apiRoutes);
+
+// Export the router
+module.exports = router;
